ccedab2b0b685334fea046fd43c4f0baea547443
narendra087 <personal.rendra@gmail.com>
2024-06-04 17:56:12 +0700

    [UPDATE] Add Email Field

--- resources/views/components/disposition-form.blade.php
+++ resources/views/components/disposition-form.blade.php
@@ -7,7 +7,7 @@
             </p>
 
             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div class="col-span-full">
+                <div class="sm:col-span-3">
                     <label for="nama-pemohon" class="block text-sm font-medium leading-6 text-gray-900">
                         Nama Pemohon <span class="text-red-500">*</span></label>
                     <div class="mt-2">
@@ -21,6 +21,14 @@
                         <span class="text-red-500">*</span></label>
                     <div class="mt-2">
                         <input type="text" name="nomor-identitas" id="nomor-identitas" autocomplete="NIP/NUP/NIM"
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div class="sm:col-span-3">
+                    <label for="email-address" class="block text-sm font-medium leading-6 text-gray-900">Alamat Email <span class="text-red-500">*</span></label>
+                    <div class="mt-2">
+                        <input type="email" name="email-address" id="email-address" autocomplete="email"
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6">
                     </div>
                 </div>
